a009f534d9f4922360c03f2eae06bf7fa4f054ff
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-06-19 13:25:27 +0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = [] #Create a new class array called @@cart to hold any items in your cart
 
   def call(env)
     resp = Rack::Response.new
@@ -10,9 +11,29 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
+
+    elsif req.path.match(/search/) #Create a new route called /search that takes in a GET param with the q item
+      search_term = req.params["q"] #GET param with the q item
       resp.write handle_search(search_term)
+
+    elsif  req.path.match(/cart/) #Create a new route called /cart to show the items in your cart
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/) #Create a new route called /add that takes in a GET param with the key item
+      add_term = req.params["item"] #GET param with the key item
+      if @@items.include?(add_term) #checking if add_term is available in list of @@items
+        @@cart << add_term #adding the item to our cart
+        resp.write "added #{add_term}"
+      else
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
